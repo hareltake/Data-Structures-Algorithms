bcacbfd36afc4ec62de96546594d8b6926fa950b
hareltake <374094103@qq.com>
2013-10-12 22:44:39 +0800

    stronger
    hehe

--- ranaizen.cc
+++ ranaizen.cc
@@ -5,119 +5,91 @@ using namespace std;
 struct polynode
 {
     double coef;
-    int exp;
-    polynode *link;
+    int exe;
+    polynode * next;
 };
-typedef polynode *polypointer;
-
-char compare(int a,int b)
-{
-    if(a>b)
-        return '>';
-    else if(a<b)
-        return '<';
-    else
-        return '=';
-}
-
-polypointer Attach(int c,int e,polypointer d)
-{
-
-    polypointer x;
-    x=new polynode;
-    x->coef=c;
-    x->exp=e;
-    d->link=x;
-    return x;
-}
-
-polypointer PolyAdd(polypointer a,polypointer b)
-{
-    polypointer p,q,d,c;
-    int x;
-    p=a;
-    q=b;
-    c=new polynode;
-    d=c;
-    while((p!=NULL)&&(q!=NULL))
-        switch(compare(p->exp,q->exp))
-        {
-        case'=':
-            x=p->coef+q->coef;
-            if(x!=0)
-                d=Attach(x,p->exp,d);
-            p=p->link;
-            q=q->link;
-            break;
-        case'>':
-            d=Attach(p->coef,p->exp,d);
-            p=p->link;
-            break;
-        case'<':
-            d=Attach(p->coef,p->exp,d);
-            q=q->link;
-            break;
-        }
-    while (p!=NULL)
-    {
-        d=Attach(p->coef,p->exp,d);
-        p=p->link;
-    }
-    while(q!=NULL)
-    {
-        d=Attach(q->coef,q->exp,d);
-        q=q->link;
-    }
-    d->link=NULL;
-    p=c;
-    c=c->link;
-    delete p;
-    return c;
-}
-
+void tailinsert(polynode *head,double a,int b);
+void frontinsert(polynode *position1,double a,int b);
+void polyadd(polynode *position1,polynode *position2);
 int main()
 {
-    polypointer a,b,c,d,e;
-    a=new polynode;
-    b=new polynode;
-    c=new polynode;
-    int coef,exp, i = 1;
-    d = a;
-    e = b;
-    while(i)
+    polynode *head1=new polynode;
+    polynode *head2=new polynode;
+    head1->next=NULL;
+    head2->next=NULL;
+    cout<<"请按如下格式输入多项式一：系数 指数（指数从高到低，以0 0结束）"<<endl;
+    double a;
+    int b;
+    cin>>a>>b;
+    while(a!=0||b!=0)
     {
-        cout<<"分别输入系数，指数：";
-       cin>>coef;
-       cout<<endl;
-       cin>>exp;
-       cout<<endl;
-       if(coef==0&&exp==0)
-        break;
-       d=Attach(coef,exp,d);
+        tailinsert(head1,a,b);
+        cin>>a>>b;
     }
-
-        cout<<"第二个:";
-
-    while(1)
+    cout<<"请按上述格式输入多项式二："<<endl;
+    cin>>a>>b;
+    while(a!=0||b!=0)
     {
-        cout<<"分别输入系数，指数：";
-       cin>>coef;
-       cout<<endl;
-       cin>>exp;
-       cout<<endl;
-       if(coef==0&&exp==0)
-        break;
-       e=Attach(coef,exp,e);
+        tailinsert(head2,a,b);
+        cin>>a>>b;
     }
-    c=PolyAdd(a->link,b->link);
-    cout<<"所得结果为:";
-    cout<<c->coef<<"x^"<<c->exp;
-    c=c->link;
-    while(c!=NULL)
+    polynode *p1=head1;
+    polynode *p2=head2;
+    while(p2->next!=NULL)
     {
-        cout<<"+"<<c->coef<<"x^"<<c->exp;
-        c=c->link;
+        if(p1->next==NULL)
+        {
+            while(p2->next!=NULL)
+            {
+                tailinsert(head1,p2->next->coef,p2->next->exe);
+                p2=p2->next;
+            }
+            break;
+        }
+        if(p1->next->exe<p2->next->exe)
+        {
+            frontinsert(p1,p2->next->coef,p2->next->exe);
+            p1=p1->next;
+            p2=p2->next;
+        }
+        else if(p1->next->exe==p2->next->exe)
+        {
+            polyadd(p1,p2);
+            p1=p1->next;
+            p2=p2->next;
+        }
+        else
+        {
+            p1=p1->next;
+        }
     }
-    cout<<endl;
-    return 0;
+    cout<<"两多项式相加如下："<<endl;
+    p1=head1;
+    while(p1->next!=NULL)
+    {
+        cout<<p1->next->coef<<"X^"<<p1->next->exe<<"+";
+        p1=p1->next;
+    }
+}
+void tailinsert(polynode *head,double a,int b)
+{
+    polynode *p=head;
+    while(p->next!=NULL)
+        p=p->next;
+    p->next=new polynode;
+    p->next->coef=a;
+    p->next->exe=b;
+    p->next->next=NULL;
+}
+void frontinsert(polynode *position1,double a,int b)
+{
+    polynode *temp=position1->next;
+    position1->next=new polynode;
+    position1->next->coef=a;
+    position1->next->exe=b;
+    position1->next->next=temp;
+}
+void polyadd(polynode *position1,polynode *position2)
+{
+    position1->next->coef+=position2->next->coef;
 }
